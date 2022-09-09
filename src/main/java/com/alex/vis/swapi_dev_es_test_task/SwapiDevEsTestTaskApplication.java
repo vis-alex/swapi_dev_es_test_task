2c7bca19ab4c17eb721dee61f095691cf7081cda
vis-alex <usenkoshurik@mail.ru>
2022-09-09 20:45:45 +0300

    Add downloading files from source

--- src/main/java/com/alex/vis/swapi_dev_es_test_task/SwapiDevEsTestTaskApplication.java
+++ src/main/java/com/alex/vis/swapi_dev_es_test_task/SwapiDevEsTestTaskApplication.java
@@ -1,8 +1,12 @@
 package com.alex.vis.swapi_dev_es_test_task;
 
+import com.alex.vis.swapi_dev_es_test_task.util.FileUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.io.File;
+import java.nio.file.Files;
 
 @SpringBootApplication
 @EnableFeignClients("com.alex.vis.swapi_dev_es_test_task.client")
@@ -10,18 +14,27 @@ public class SwapiDevEsTestTaskApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SwapiDevEsTestTaskApplication.class, args);
-    }
 
-}
+        FileUtil.downloadData("https://swapi.dev/api/planets?format=json");
 
-//FileUtil.downloadFile(new URL(
-//        "https://swapi.dev/api/planets?format=json"),
-//        "E:\\Projects\\swapi_dev_es_test_task\\planets.json"
-//        );
-//        try {
-//             json = new JSONObject(jsonString);
-//            int someInt = json.getInt("someInt");
-//            String someString = json.getString("someString");
-//        } catch (JSONException e) {
-//            //
+
+//        FileUtil.downloadFile(
+//                "https://swapi.dev/api/planets?format=json",
+//                "planets.json",
+//                false);
+//        FileUtil.writePlanetsAsJsonToFile(FileUtil.getJsonObjectFromFile("planets.json"));
+//
+//
+//        long result = FileUtil.getResultCount("planets.json");
+//        System.out.println(result);
+//
+//        if (result > 10) {
+//            long countPages = (long)Math.ceil(result / 10);
+//            System.out.println("countPages - " + countPages);
+//
 //        }
+
+
+
+    }
+}
--- src/main/java/com/alex/vis/swapi_dev_es_test_task/util/FileUtil.java
+++ src/main/java/com/alex/vis/swapi_dev_es_test_task/util/FileUtil.java
@@ -1,20 +1,83 @@
 package com.alex.vis.swapi_dev_es_test_task.util;
 
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
 
 public final class FileUtil {
+    public static final String DOWNLOADED_FILES_DIRECTORY = "src/main/resources/downloaded_files";
+    private static int count = 0;
+    private static final JSONParser parser = new JSONParser();
 
     private FileUtil() {}
 
-    /**If you don`t need to replace file with restart the application
-     - deleteStandardCopyOption.REPLACE_EXISTING parameter */
-    public static void downloadFile(URL url, String fileName) throws Exception {
-        try (InputStream in = url.openStream()) {
-            Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+    public static void downloadData(String url) throws Exception {
+        File planetsDirectory = new File(DOWNLOADED_FILES_DIRECTORY);
+        String filePath = planetsDirectory.getPath() + "/" + (++count) + ".json";
+        downloadFile(url,  filePath , false);
+        downloadPlanets(FileUtil.getJsonObjectFromFile(filePath));
+    }
+
+    public static void downloadFile(String urlPath, String fileName, boolean append) throws Exception {
+        try {
+            URI url = new URI(urlPath);
+
+            HttpURLConnection connection = (HttpURLConnection) url.toURL().openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+
+            try (FileWriter file = new FileWriter(fileName, append)) {
+                file.write(response.toString());
+                file.flush();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    //TODO THROW PARSE EXCEPTION
+    public static JSONObject getJsonObjectFromFile(String fileName) {
+
+        try (FileReader reader = new FileReader(fileName)){
+            return (JSONObject) parser.parse(reader);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void downloadPlanets(JSONObject rootJsonObject) throws Exception {
+        String nextPageUrl;
+        if (rootJsonObject.get("next") != null) {
+            nextPageUrl = rootJsonObject.get("next").toString();
+            System.out.println(nextPageUrl);
+
+            String fileName = DOWNLOADED_FILES_DIRECTORY + "/" + (++count) + ".json";
+
+            downloadFile(
+                    nextPageUrl,
+                    fileName,
+                    false
+            );
+
+            JSONObject nextPageJsonObject = getJsonObjectFromFile(fileName);
+            downloadPlanets(nextPageJsonObject);
         }
     }
+
 }
